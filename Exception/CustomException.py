b75638daec14ed0c8ed464d0978a3caedac121b0
a1oyss <2697963615@qq.com>
2021-03-26 23:07:14 +0800

    添加自定义异常

---
+++ Exception/CustomException.py
@@ -0,0 +1,26 @@
+class ServiceError(Exception):
+    pass
+
+
+class RequestError(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
+
+
+class NetworkError(Exception):
+    pass
+
+
+class IndexError(Exception):
+    pass
+
+
+class OutOfSearch(Exception):
+    pass
+
+
+class TooQuick(Exception):
+    pass
