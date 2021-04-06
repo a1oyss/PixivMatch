25b6706bd8431549f775d7beef6b4ad4bbf90514
a1oyss <2697963615@qq.com>
2021-04-06 09:24:50 +0800

    修改连接redis属性

--- utils/redisConn.py
+++ utils/redisConn.py
@@ -3,7 +3,7 @@ import redis
 class Redis:
     def __init__(self, host='localhost', port=6379, db=0, password=None):
         self.__conn = redis.Redis(connection_pool=redis.BlockingConnectionPool(
-            max_connections=15, host=host, port=port, db=db, password=password),decode_responses=True)
+            max_connections=15, host=host, port=port, db=db, password=password, decode_responses=True))
 
     def __getattr__(self, command):
         def _(*args):
